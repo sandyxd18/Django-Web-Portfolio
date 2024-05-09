954137b552ae862fc454b6a5a6113dc5dcf766da
sandyxd18 <sandyidn04@gmail.com>
2024-05-09 07:37:07 +0700

    change settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+199t(1)@y^t-=-bm@h6oy$$-vo-pgefjd2=%+iqb2sc_jzq6+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
